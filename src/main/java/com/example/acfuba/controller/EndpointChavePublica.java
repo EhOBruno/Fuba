8afc0356f207de95c25c53593ca1141753e32561
Bruno Ricardo de Menezes <bruno.menezes@iti.gov.br>
2024-04-19 14:16:44 -0300

    fuba armazenando token e chaves

--- src/main/java/com/example/acfuba/controller/EndpointChavePublica.java
+++ src/main/java/com/example/acfuba/controller/EndpointChavePublica.java
@@ -11,23 +11,38 @@ import com.example.acfuba.crypto.CalculaChavePublica;
 import com.example.acfuba.util.GerarPrimoAleatorio;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class EndpointChavePublica {
+
+    // Mapa para armazenar o token e as chaves públicas geradas
+    private static Map<String, BigInteger[]> chavesPublicasMap = new HashMap<>();
 
     public static void main(String[] args) {
         SpringApplication.run(EndpointChavePublica.class, args);
     }
 
     @GetMapping("/chavesPublicas")
-    public String obterChavesPublicas(@RequestParam(name = "token") BigInteger token) {
+    public String obterChavesPublicas(@RequestParam(name = "token") String token) {
+
+        System.out.println(token);
+
         // Verifica se o token está presente na URL (Normalmente nem chega nessa validação caso não tenha token)
         if(ObjectUtils.isEmpty(token)) {
             return "Token não fornecido na URL";
         }
 
-        // Aqui você pode adicionar lógica para validar o token, se necessário
+        // Verifica se o token já existe no mapa
+        if(chavesPublicasMap.containsKey(token)) {
+            // Se o token já existir, retorna as chaves públicas previamente geradas
+            BigInteger[] chavesArmazenadas = chavesPublicasMap.get(token);
+            BigInteger e = chavesArmazenadas[0];
+            BigInteger n = chavesArmazenadas[1];
+            return "Chaves públicas (recuperadas): (" + e + ", " + n + ")";
+        }
 
         int numDigitos = 10; // Número de dígitos para cada número primo
 
@@ -44,7 +59,11 @@ public class EndpointChavePublica {
         // Calcular a segunda chave pública "e"
         BigInteger e = CalculaChavePublica.calcularChavePublicaE(fiN);
 
+        // Armazenar as chaves públicas no mapa
+        BigInteger[] chaves = {e, n};
+        chavesPublicasMap.put(token, chaves);
+
         // Retornar as chaves públicas no formato desejado
-        return "Chaves públicas: (" + e + ", " + n + ")";
+        return "Chaves públicas (geradas): (" + e + ", " + n + ")";
     }
 }
---
+++ src/main/java/com/example/acfuba/controller/JSONObject.java
@@ -0,0 +1,5 @@
+package com.example.acfuba.controller;
+
+public class JSONObject {
+
+}
